6cbcd82476aeb1d78a98a7421ac5fd3b979e4f9b
Munraizu <128634406+Munraizu@users.noreply.github.com>
2023-10-14 12:41:16 +0500

    Update kr2_Afanaseva.R

--- kr2_Afanaseva.R
+++ kr2_Afanaseva.R
@@ -1,53 +1,40 @@
-install.packages("igraph")
-install.packages("ggplot2")
-
 library(igraph)
 library(ggplot2)
+install.packages("gridExtra")
+library(gridExtra)
 
-generate_edgelist <- function(num_vertices, num_edges) {
-  edges <- matrix(0, nrow = num_edges, ncol = 2)
-  for (i in 1:num_edges) {
-    edges[i, 1] <- sample(1:num_vertices, 1)
-    edges[i, 2] <- sample(1:num_vertices, 1) 
-  }
-  edgelist <- data.frame(from = edges[, 1], to = edges[, 2])
-  return(edgelist)
-}
+generate_edgelist <- function(num_vertices, num_edges) { 
+  edges <- matrix(0, nrow = num_edges, ncol = 2) 
+  for (i in 1:num_edges) { 
+    edges[i, 1] <- sample(1:num_vertices, 1) 
+    edges[i, 2] <- sample(1:num_vertices, 1)  
+  } 
+  edgelist <- data.frame(from = edges[, 1], to = edges[, 2]) 
+  return(edgelist) 
+} 
 
+create_graph <- function(edgelist) { 
+  graph <- graph_from_edgelist(as.matrix(edgelist)) 
+  return(graph) 
+} 
 
-create_graph <- function(edgelist) {
-  graph <- graph_from_edgelist(as.matrix(edgelist))
-  return(graph)
-}
+edgelist <- generate_edgelist(10, 15) 
+graph <- create_graph(edgelist) 
 
-visualize_graph <- function(graph) {
-  plot(graph, layout = layout.circle, vertex.color = "blue", edge.color = "black", vertex.label.cex = 0.8)
-  legend("topleft", legend = "Укладка 1", pch = 21, col = "blue", pt.bg = "blue", bg = "white", cex = 0.8)
-  
-  plot(graph, layout = layout.fruchterman.reingold, vertex.color = "red", edge.color = "black", vertex.label.cex = 0.8)
-  legend("topleft", legend = "Укладка 2", pch = 21, col = "red", pt.bg = "red", bg = "white", cex = 0.8)
-  
-  plot(graph, layout = layout.kamada.kawai, vertex.color = "green", edge.color = "black", vertex.label.cex = 0.8)
-  legend("topleft", legend = "Укладка 3", pch = 21, col = "green", pt.bg = "green", bg = "white", cex = 0.8)
-}
+graph_circle <- graph
+graph_fr <- graph
+graph_kk <- graph
 
-save_adjacency_matrix <- function(graph, file_name) {
-  adjacency_matrix <- as_adjacency_matrix(graph)
-  write.table(adjacency_matrix, file = file_name, sep = "\t", quote = FALSE)
-}
+layout_circle <- layout.circle(graph_circle)
+layout_fr <- layout.fruchterman.reingold(graph_fr)
+layout_kk <- layout.kamada.kawai(graph_kk)
 
-save_graph_plot <- function(graph, file_name, format) {
-  png(file_name, width = 800, height = 600)
-  plot(graph, layout = layout.fruchterman.reingold, vertex.color = "red", edge.color = "black", vertex.label.cex = 0.8)
-  dev.off()
-}
+p1 <- plot(graph_circle, layout = layout_circle, vertex.color = "blue", edge.color = "black", vertex.label.cex = 0.8)
+p2 <- plot(graph_fr, layout = layout_fr, vertex.color = "red", edge.color = "black", vertex.label.cex = 0.8)
+p3 <- plot(graph_kk, layout = layout_kk, vertex.color = "green", edge.color = "black", vertex.label.cex = 0.8)
 
-edgelist <- generate_edgelist(10, 15)
+multiplot(p1, p2, p3, cols = 3)
 
-graph <- create_graph(edgelist)
-
-visualize_graph(graph)
-
-save_adjacency_matrix(graph, "adjacency_matrix.txt")
-save_graph_plot(graph, "graph_plot.png", format = "png")
+save_adjacency_matrix(edgelist, "edgelist.txt")
+save_graph_plot(p1, "graph.png", format = "png")
 
